d7bd35c9c7185d425338b61ecd7d81f387227e9f
dyevtyushkin <dyevtyushkin@evolution.com>
2024-02-14 09:03:51 +0000

    V2: Works on preset split view

--- lib/project/ui/project_page_preset_split_view_tab.dart
+++ lib/project/ui/project_page_preset_split_view_tab.dart
@@ -4,27 +4,15 @@ import 'package:resizable_widget/resizable_widget.dart';
 
 /// A widget that displays a tab of a project page split view.
 class ProjectPagePresetSplitViewTab extends HookWidget {
-  /// A flag that indicates whether this tab is a sub-tab.
-  final bool isSubTab;
-
   /// Returns a new instance of the [ProjectPagePresetSplitViewTab].
   const ProjectPagePresetSplitViewTab({
-    this.isSubTab = false,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (isSubTab) {
-      return const Placeholder();
-    }
-
     final subTabs = useState(
-      const <Widget>[
-        ProjectPagePresetSplitViewTab(
-          isSubTab: true,
-        ),
-      ],
+      const <Widget>[],
     );
 
     return ResizableWidget(
---
+++ lib/project/ui/project_page_preset_split_view_tab_tuning.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// A widget that displays the tuning tab of a project page split view.
+class ProjectPagePresetSplitViewTabTuning extends StatelessWidget {
+  /// Returns a new instance of the [ProjectPagePresetSplitViewTabTuning].
+  const ProjectPagePresetSplitViewTabTuning({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
